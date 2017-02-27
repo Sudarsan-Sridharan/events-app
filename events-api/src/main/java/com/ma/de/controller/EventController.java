15e32a00512aba6f55dd7acf292e854ea22dfef2
Mariusz Dekarski <ma-de@tlen.pl>
2017-02-27 23:33:09 +0000

    Added React call to api, added cross-origin to api

--- events-api/src/main/java/com/ma/de/controller/EventController.java
+++ events-api/src/main/java/com/ma/de/controller/EventController.java
@@ -4,9 +4,7 @@ import com.ma.de.domain.Event;
 import com.ma.de.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -21,7 +19,8 @@ public class EventController {
     @Autowired
     private EventService eventService;
 
-    @RequestMapping("/events/{location}")
+    @CrossOrigin
+    @GetMapping("/events/{location}")
     public List<Event> getEventsForLocation(@PathVariable String location) {
 
         if(StringUtils.isEmpty(location)){
--- events-web/src/main/js/app.js
+++ events-web/src/main/js/app.js
@@ -2,16 +2,69 @@
 
 // tag::vars[]
 const React = require('react');
-const ReactDOM = require('react-dom')
-//const client = require('./client');
+const ReactDOM = require('react-dom');
+const client = require('./client');
 // end::vars[]
 
 // tag::app[]
+class App extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {events: []};
+	}
+
+	componentDidMount() {
+		client({method: 'GET', path: 'http://localhost:8080/events/Brighton'}).done(response => {
+			this.setState({events: response.entity});
+		});
+	}
+
+	render() {
+		return (
+			<EventsList events={this.state.events}/>
+		)
+	}
+}
+
+
+
+class EventsList extends React.Component{
+	render() {
+		var events = this.props.events.map(event =>
+			<Event key={event.id} event={event}/>
+		);
+
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>Title</th>
+						<th>Address</th>
+						<th>Date</th>
+					</tr>
+					{events}
+				</tbody>
+			</table>
+		)
+	}
+}
+
+class Event extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.event.title}</td>
+				<td>{this.props.event.venue_address}</td>
+				<td>{this.props.event.start_time}</td>
+			</tr>
+		)
+	}
+}
 
 
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('react')
 );
